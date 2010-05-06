73ae759555fb6251debcc65422a76954715b89b3
Bethany M Soule <bsoule@unknown-10-101-7-158.yahoo.com>
2010-05-05 21:52:04 -0400

    runs with button now

--- src/pro/ba/BillyGrid.java
+++ src/pro/ba/BillyGrid.java
@@ -15,62 +15,58 @@ public class BillyGrid extends Activity {
 	private int billySize = 20;
 	private int nCols;
 	private int nRows;
- 	private static String TAG = "BILLY";
- 	private Random rr = new Random();
+	private static String TAG = "BILLY";
+	private Random rr = new Random();
 	private TableLayout billyGrid;
- 	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.grid);
-        
-        nCols = (int) Math.floor(Math.sqrt(billySize)); //TODO: test for Portrait vs Landscape and switch floor/ceil accordingly
-        nRows = (int) Math.ceil((double)billySize/nCols);       
-        
-        billyGrid = (TableLayout) findViewById(R.id.billygrid);
-        	
-        //billyGrid = createBillyGrid();
-        //updateBillyGrid(0);
-    }
-    
-    // call this to refresh layout (tablelayout) with new probability
-    void updateBillyGrid(double p) {
-    	boolean[] rands = RandomKSubset(billySize, p);
-    	for (int i=0; i<billyGrid.getChildCount(); i++) {
-    		TableRow tr = (TableRow) billyGrid.getChildAt(i);
-    		for (int j=0; j<tr.getChildCount(); j++) {
-    			TextView tv = (TextView) tr.getChildAt(j);
-    			tv.requestLayout();
-    			if ( rands[i*nCols + j] ) {
-    				tv.setBackgroundColor(Color.DKGRAY);
-    			} else {
-    				tv.setBackgroundColor(Color.BLACK);
-    			}
-    		}
-    	}
-    	billyGrid.requestLayout();
-    }
-    
-    
-    // make the billyGrid. start with all views false.
-    private TableLayout createBillyGrid() {
-    	TableLayout table = new TableLayout(this);
-    	ArrayList<TableRow> bg = new ArrayList<TableRow>();
-        TableRow row = null;
-        for (int i=0; i<billySize; i++) {
-        	if (i % nCols == 0) {
-        		bg.add(new TableRow(this));
-        		row = bg.get(bg.size()-1);
-        	}
-        	row.addView(billyGetView(i));
-        }
-        for (TableRow r : bg) 
-        	table.addView(r);
-        table.setStretchAllColumns(true);
-        return table;
-    }
-    
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.grid);
+
+		nCols = (int) Math.floor(Math.sqrt(billySize)); //TODO: test for Portrait vs Landscape and switch floor/ceil accordingly
+		nRows = (int) Math.ceil((double)billySize/nCols);       
+
+		billyGrid = (TableLayout) findViewById(R.id.billygrid);
+		createBillyGrid();
+		updateBillyGrid(0);
+	}
+
+	// call this to refresh layout (tablelayout) with new probability
+	void updateBillyGrid(double p) {
+		boolean[] rands = RandomKSubset(billySize, p);
+		for (int i=0; i<billyGrid.getChildCount(); i++) {
+			TableRow tr = (TableRow) billyGrid.getChildAt(i);
+			for (int j=0; j<tr.getChildCount(); j++) {
+				TextView tv = (TextView) tr.getChildAt(j);
+				tv.requestLayout();
+				if ( rands[i*nCols + j] ) {
+					tv.setBackgroundColor(Color.DKGRAY);
+				} else {
+					tv.setBackgroundColor(Color.BLACK);
+				}
+			}
+		}
+		billyGrid.requestLayout();
+	}
+
+
+	// make the billyGrid. start with all views false.
+	private void createBillyGrid() {
+		//TableLayout table = new TableLayout(this);
+		//ArrayList<TableRow> bg = new ArrayList<TableRow>();
+		TableRow row = null;
+		for (int i=0; i<billySize; i++) {
+			if (i % nCols == 0) {
+				billyGrid.addView(new TableRow(this));
+				row = (TableRow) billyGrid.getChildAt(billyGrid.getChildCount()-1);
+			}
+			row.addView(billyGetView(i));
+		}
+		billyGrid.setStretchAllColumns(true);
+	}
+
 	TextView billyGetView(int n) {
 		TextView tv = new TextView(this);
 		tv.setText(""+n);
@@ -80,14 +76,14 @@ public class BillyGrid extends Activity {
 
 		return tv;
 	}
-	
+
 	// return a set of lit/unlit for each digit [0..n-1] 
 	// s.t. the probability of any prechosen number [0..n-1]
 	// being lit up is p. (eg p=1/2 then light up half at random) 
 	boolean[] RandomKSubset(int n, double p) {
-    	int q = ( rr.nextDouble() < p*billySize - Math.floor(p*billySize) ) ? 1 : 0;
-    	int nlit = (int) Math.floor(p*billySize) + q; // 'k'
-		
+		int q = ( rr.nextDouble() < p*billySize - Math.floor(p*billySize) ) ? 1 : 0;
+		int nlit = (int) Math.floor(p*billySize) + q; // 'k'
+
 		double needed = nlit*1.0;
 		boolean[] result = new boolean[n];
 		for (int i=0; i<n; i++) {
