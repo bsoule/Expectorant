b472ee323fc0dd73b66486c87d09ab258c79a9c3
Bethany M Soule <bsoule@aconite.local>
2010-05-07 14:56:11 -0400

    matheval changes in billyGrid. style changes in grid/manifest

--- src/pro/ba/BillyGrid.java
+++ src/pro/ba/BillyGrid.java
@@ -1,68 +1,85 @@
 package pro.ba;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BillyGrid extends Activity {
 	private int billySize = 20;
 	private int nCols;
-	private int nRows;
-	private static String TAG = "BILLY";
-	private Random rr = new Random();
 	private TableLayout billyGrid;
-
+	private Button billyButton;
+	private EditText billyProb;
+	private int billyUnLit = Color.DKGRAY;
+	private boolean[] billySubset;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.grid);
-
+		
 		nCols = (int) Math.floor(Math.sqrt(billySize)); //TODO: test for Portrait vs Landscape and switch floor/ceil accordingly
-		nRows = (int) Math.ceil((double)billySize/nCols);       
 
+		billyProb = (EditText) findViewById(R.id.billyprob);
+		billyProb.setHint("Enter an expression");
+		billyButton = (Button) findViewById(R.id.billybutton);
+		billyButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				try {
+					double p = BillyTilities.arithmeval(billyProb.getText().toString());
+					billySubset = BillyTilities.BillySubset(billySize, p);
+					updateBillyGrid(billySubset);
+					//Toast.makeText()
+				} catch (Exception e) {
+					Toast.makeText(v.getContext(), "There seems to be an error in your expression.", Toast.LENGTH_SHORT);
+				}
+			}
+		});
+		
 		billyGrid = (TableLayout) findViewById(R.id.billygrid);
 		createBillyGrid();
-		updateBillyGrid(0);
+		billySubset = BillyTilities.BillySubset(billySize, 0);
+		updateBillyGrid(billySubset);
 	}
 
 	// call this to refresh layout (tablelayout) with new probability
-	void updateBillyGrid(double p) {
-		boolean[] rands = RandomKSubset(billySize, p);
+	void updateBillyGrid(boolean[] bsub) {
 		for (int i=0; i<billyGrid.getChildCount(); i++) {
 			TableRow tr = (TableRow) billyGrid.getChildAt(i);
 			for (int j=0; j<tr.getChildCount(); j++) {
 				TextView tv = (TextView) tr.getChildAt(j);
-				tv.requestLayout();
-				if ( rands[i*nCols + j] ) {
-					tv.setBackgroundColor(Color.DKGRAY);
+				if ( bsub[i*nCols + j] ) {
+					tv.setTextColor(Color.WHITE);
 				} else {
-					tv.setBackgroundColor(Color.BLACK);
+					tv.setTextColor(billyUnLit);
 				}
+				tv.requestLayout();
 			}
 		}
 		billyGrid.requestLayout();
 	}
 
-
 	// make the billyGrid. start with all views false.
 	private void createBillyGrid() {
-		//TableLayout table = new TableLayout(this);
-		//ArrayList<TableRow> bg = new ArrayList<TableRow>();
 		TableRow row = null;
 		for (int i=0; i<billySize; i++) {
 			if (i % nCols == 0) {
 				billyGrid.addView(new TableRow(this));
 				row = (TableRow) billyGrid.getChildAt(billyGrid.getChildCount()-1);
 			}
-			row.addView(billyGetView(i));
+			row.addView(billyGetView(i+1));
 		}
 		billyGrid.setStretchAllColumns(true);
 	}
@@ -73,28 +90,9 @@ public class BillyGrid extends Activity {
 		tv.setGravity(Gravity.CENTER);
 		tv.setTextSize(30);
 		tv.setPadding(0,5,0,5);
+		tv.setTextColor(billyUnLit);
 
 		return tv;
-	}
-
-	// return a set of lit/unlit for each digit [0..n-1] 
-	// s.t. the probability of any prechosen number [0..n-1]
-	// being lit up is p. (eg p=1/2 then light up half at random) 
-	boolean[] RandomKSubset(int n, double p) {
-		int q = ( rr.nextDouble() < p*billySize - Math.floor(p*billySize) ) ? 1 : 0;
-		int nlit = (int) Math.floor(p*billySize) + q; // 'k'
-
-		double needed = nlit*1.0;
-		boolean[] result = new boolean[n];
-		for (int i=0; i<n; i++) {
-			if ( rr.nextDouble() < needed/(n-i) ) {
-				result[i] = true;
-				needed--;
-			} else {
-				result[i] = false;
-			}
-		}
-		return result;
 	}
 
 }
