92596364ab6a50d39928522463e3ba4695c14480
Daniel Reeves <dreev.es@gmail.com>
2022-12-19 21:17:14 +0000

    Lots of simplification/refactoring

--- spinner.js
+++ spinner.js
@@ -1,3 +1,11 @@
+/* Here we provide the following functions for spinning a spinner:
+1. genslots: generate the list of slots aka pie slices
+2. spinit: initialize spinner with a div and list of slots, return spin object
+3. spingo: make it start spinning, destined to land on given slot
+4. spinup: update the spinner with new slots (and redraw it)
+5. spindraw: redraw the spinner
+*/
+
 // -----------------------------------------------------------------------------
 const max = Math.max
 const min = Math.min
@@ -20,7 +28,8 @@ function mod(x, m) { return (x % m + m) % m }
 
 // Convert Polar to Cartesian coordinates
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  const angleInRadians = (angleInDegrees-90) * TAU / 360.0
+  angleInDegrees = mod(angleInDegrees, 360)
+  const angleInRadians = (angleInDegrees-90)/360*TAU
   return { x: centerX + (radius * cos(angleInRadians)),
            y: centerY + (radius * sin(angleInRadians)) }
 }
@@ -32,7 +41,8 @@ function arcPath(x, y, radius, endradius, startAngle, endAngle) {
   const start2 = polarToCartesian(x, y, endradius, endAngle)
   const end2   = polarToCartesian(x, y, endradius, startAngle)
   const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1"
-  ASSERT(!isNaN(start.x), `start.x is NaN: ${start.x}`)
+  ASSERT(!isNaN(start.x) && !isNaN(start2.x) && !isNaN(end.x) &&! isNaN(end2.x),
+    `arcPath(${x}, ${y}, ${radius}, ${endradius}, ${startAngle}, ${endAngle})`)
   return [
     "M", start.x, start.y, 
     "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y,
@@ -69,28 +79,28 @@ tolerance on what counts as stopped.
 And for posterity, here's a sine-based easing function we found on the internet
 but it's pretty annoying to work with:
 
-function computeRotation(duration, startTime, startSpeed, currentTime) {
-  const t = currentTime - startTime
+function rotationFromTime(duration, tini, startSpeed, tcur) {
+  const t = tcur - tini
   return startSpeed * 
     (duration*cos(TAU/4 * t / duration)/(TAU/4) + duration + t) - 
       startSpeed * duration * (4/TAU + 1)
 }
 
-function computeSpeed(duration, startTime, startSpeed, currentTime) {
-  const t = currentTime - startTime
+function speedFromTime(duration, tini, startSpeed, tcur) {
+  const t = tcur - tini
   return -cos((1 - t/duration) * TAU/4) * startSpeed + startSpeed
 }
 */
 
 // Stop a tiny bit before the real target so it snaps into place?
-function computeTotalRotation(duration, startTime, startSpeed) {
-  const currentTime = startTime + duration - 0.01
-  return computeRotation(duration, startTime, startSpeed, currentTime)
+function totalRotation(duration, tini, startSpeed) {
+  const tcur = tini + duration - 0.01
+  return rotationFromTime(duration, tini, startSpeed, tcur)
 }
 
 // Compute rotation as a function of current time
-function computeRotation(duration, startTime, startSpeed, currentTime) {
-  const t = currentTime - startTime
+function rotationFromTime(duration, tini, startSpeed, tcur) {
+  const t = tcur - tini
   return -startSpeed/(2.5*duration)*(t-duration)**2 + startSpeed*duration/2.5
 
   const T = duration
@@ -100,7 +110,7 @@ function computeRotation(duration, startTime, startSpeed, currentTime) {
 }
 
 // I'm confused; time as a function of distance should be messier than this...
-function computeTimeFromRotation(duration, startSpeed, rotation) {
+function timeFromRotation(duration, startSpeed, rotation) {
   return 2.5*rotation / startSpeed
 
   const T = duration
@@ -110,8 +120,8 @@ function computeTimeFromRotation(duration, startSpeed, rotation) {
 }
 
 // Derivative of the distance function
-function computeSpeed(duration, startTime, startSpeed, currentTime) {
-  const t = currentTime - startTime
+function speedFromTime(duration, tini, startSpeed, tcur) {
+  const t = tcur - tini
   return 0.8 * startSpeed * (duration - t) / duration
   
   const T = duration
@@ -122,55 +132,47 @@ function computeSpeed(duration, startTime, startSpeed, currentTime) {
 
 // Execute the animation frame using css
 function spin_anim(spin) {
-  if (spin.speed < 0.01) { setTimeout(spin_stop(spin), 0); return true }
+  if (spin.speed < 0.01) { setTimeout(spinstop(spin), 0); return true }
   const t = Date.now()
-  spin.speed = computeSpeed(spin.duration, spin.startTime, spin.spinSpeed, t)
-  // the spin is being eased, but what we want is the current location
-  spin.degree = computeRotation(spin.duration, spin.startTime,spin.spinSpeed,t)
-	spin.degree = mod(spin.degree, 360)
-  spin.obj.style.transform = `rotate(-${spin.degree}deg)`
+  spin.speed = speedFromTime(spin.duration, spin.tini, spin.spinSpeed, t)
+  // The spin is being eased, but what we want is the current location
+  spin.degree = rotationFromTime(spin.duration, spin.tini, spin.spinSpeed, t)
+  spin.obj.style.transform = `rotate(-${mod(spin.degree, 360)}deg)`
 	requestAnimFrame(() => spin_anim(spin)) // curried version of spin_anim? why?
 }
 
 // Take a spin object and the index of the slot destined to win, start spinning
-function spin_start(spin, winner) {
-  ASSERT(winner >= 0 && winner < spin.slots.length, 
-    `Can't take slot ${winner} of ${JSON.stringify(spin.slots)}`)
+function spingo(spin, windex) {
+  ASSERT(windex >= 0 && windex < spin.slots.length, 
+    `Can't take slot ${windex} of ${JSON.stringify(spin.slots)}`)
   spin.degree = 0
-  spin.winner = winner
+  spin.windex = windex
   spin.obj.style.transform = `rotate(-${spin.degree}deg)`
-  spin.startTime = Date.now()
+  spin.tini = Date.now()
 	spin.rand_speed = Math.random()
   spin.speed = spin.spinSpeed
   spin.duration = spin.min_duration + 
-    (spin.max_duration - spin.min_duration) * spin.rand_speed
+                       (spin.max_duration - spin.min_duration) * spin.rand_speed
 
   // Compute the total rotation, and figure out if it is landing on the winner.
   // If not, adjust the duration so that we do land on the winner by 
   // determining a position and then computing the time at that position and
-  // updating the duration to match.
+  // updating the duration to match. This is not elegant.
 
-  // 1 - Figure out the min and max angle/rotation for the winner
-  const win = spin.slots[winner] //start/endAngle
-  // 2 - Compute the rotation angles for the given duration
-  const rotationTot = 
-    computeTotalRotation(spin.duration, spin.startTime, spin.spinSpeed)
+  const win = spin.slots[windex]
+  const rotationTot = totalRotation(spin.duration, spin.tini, spin.spinSpeed)
   const oneRot = mod(rotationTot, 360)
-  // 3 - Figure out if we're already there, or if we need to move things around
-  if (oneRot < win.startAngle || oneRot > win.endAngle) {
-    // not inside the winner, so determine how much more we need by finding
+  // Figure out if we're already on the winner, or if we need to adjust
+  const p = win.weight
+  let [a, b] = [(win.kyoom - p) * 360, win.kyoom * 360]  // start & end angles
+  if (oneRot < a || oneRot > b) {
+    // Not inside the winner, so determine how much more we need by finding
     // a random value between start and end
-    const ranAngle = 
-                Math.random() * (win.endAngle - win.startAngle) + win.startAngle
-    // decide how much to add to oneRot to get to ranAgnle, then do that to
-    // rotationTot instead
-    const newTot = rotationTot + (ranAngle - oneRot)
-    // newTot should now be valid, so we need to compute a duration that can 
-    // get us to this total.
-    // Hmm, but shouldn't duration always be the length of the sound clip?
-    const newTime = 
-      computeTimeFromRotation(spin.duration, spin.spinSpeed, newTot)
-    spin.duration = newTime
+    const ranAngle = Math.random() * (b - a) + a
+    // Decide how much to add to oneRot to get to ranAngle, then do that to
+    // rotationTot instead, computing a duration that gets us to the new total.
+    spin.duration = timeFromRotation(spin.duration, spin.spinSpeed, 
+                                     rotationTot + (ranAngle - oneRot))
   }
 	spin_anim(spin)
 }
@@ -178,23 +180,15 @@ function spin_start(spin, winner) {
 // Snap to the final destination (which should be close enough to where the 
 // animation is that there's no visible snapping) and draw a bolder outline 
 // around the winning pie slice.
-function spin_stop(spin) {
-  spin.degree = mod(
-       computeTotalRotation(spin.duration, spin.startTime, spin.spinSpeed), 360)
-  spin.obj.style.transform = `rotate(-${spin.degree}deg)`
-  const win = spin.slots[spin.winner]
+function spinstop(spin) {
+  spin.degree = totalRotation(spin.duration, spin.tini, spin.spinSpeed)
+  spin.obj.style.transform = `rotate(-${mod(spin.degree, 360)}deg)`
+  const win = spin.slots[spin.windex]
   const p = win.weight
-  let [a, b] = [win.startAngle, win.endAngle]
+  let [a, b] = [(win.kyoom - p) * 360, win.kyoom * 360]  // start & end angles
   if (p > 1-1e-4) { a = 0; b = 360 - 1e-4 }
   spin.obj.innerHTML += `<path d="${arcPath(50,50, 5, 50, a, b)}" ` +
                            `fill="#00000000" stroke="white" stroke-width="2" />`
-}
-
-function setSpinnerNaN(spin) {
-  const slots = compute2Slots(1)
-  slots[0].color = "black"
-  slots[0].value = "🍌"  // why a banana? idk, could use confused-emoji instead?
-  updateSpinner(spin, slots)
 }
 
 // Take the weight (probability) of the pie slice and make the font blurb
@@ -207,13 +201,18 @@ function fontblurb(x) {
 const alignblurb = 'alignment-baseline="central" text-anchor="middle" '
 const strokeblurb = 'stroke="#000000" stroke-width="1" opacity="0.3" '
 
-// Take start angle a, end angle b, the color, and the probability; return svg 
-// blurb for wedge
-function arcblurb(a, b, color, p) {
-  //CLOG(`DEBUG: arcblurb: a=${a}, b=${b}, color=${color}, p=${p}`)
-  if (a === b && color === 'black') { a = 0; b = 360 - 1e-5 }
-  if (p > 1-1e-4) { a = 0; b = 360 - 1e-4 } // weirdness with middle dot...
-  return `<path d="${arcPath(50,50, 5,50, a, b)}" fill="${color}" />`
+// Take a slot object and generate the svg blurb
+function arcblurb(slot) {
+  CLOG(`DEBUG: arcblurb slot=${JSON.stringify(slot)}`)
+  const p = slot.weight                        // probability ie fraction of pie
+  let [a, b] = [(slot.kyoom - p) * 360, slot.kyoom * 360]  // start & end angles
+  // Things break if we try to draw an arc from exactly 0 to 360 degrees, those
+  // being the same thing, so adjust to 0 to 359.999 in that case.
+  // But also for some buggy reason, things like .00001 degrees to 360 degrees
+  // also break so we're just drawing the whole circle if a pie slice is close
+  // enough to p=1.
+  if (p > 1-1e-4) { a = 0; b = 360 - 1e-4 }
+  return `<path d="${arcPath(50,50, 5,50, a, b)}" fill="${slot.color}" />`
   // We can just make an arc from 0 to almost-360 degrees so no need for this
   // special case:
   // `<circle cx="50" cy="50" r="50" fill="${color}"/>` +
@@ -223,7 +222,7 @@ function arcblurb(a, b, color, p) {
 // Take angle in degrees and x,y coordinates, return svg blurb for the rotation
 function rotateblurb(d, x, y) { return `transform="rotate(${d}, ${x}, ${y})"` }
 
-function redrawSpinner(spin) {
+function spindraw(spin) {
   const totweight = spin.slots.reduce((a, b) => a + b.weight, 0)
   if (abs(totweight - 1) > 1e-9) {
     CLOG(`TODO: ASSERT: slot weights sum to ${totweight} (should be 1)`)
@@ -240,9 +239,7 @@ function redrawSpinner(spin) {
     const midAngle = ang+degDelta/2 // draw the text in the middle of the arc
     const tc = polarToCartesian(50, 50, textOffset, midAngle) // text coords
     const rotateblurb = `transform="rotate(${midAngle}, ${tc.x}, ${tc.y})"`
-    spin.slots[i]["startAngle"] = ang
-    spin.slots[i]["endAngle"] = ang + degDelta
-    svg += arcblurb(ang, ang + degDelta, spin.slots[i].color, p) +
+    svg += arcblurb(spin.slots[i]) +
            `<text x="${tc.x}" y="${tc.y}" fill="white" ` +
                   fontblurb(p) + alignblurb + rotateblurb + '>' +
               spin.slots[i].value + '</text>'
@@ -251,23 +248,24 @@ function redrawSpinner(spin) {
   spin.obj.innerHTML = svg
 }
 
-function updateSpinner(spin, slots) {
+function spinup(spin, slots) {
   spin.slots = slots
   if (spin.slots.length < 2) throw "You must specify at least two options"
-  redrawSpinner(spin)
+  spindraw(spin)
 }
 
-// Create an svg directly in the div and initialize the spinner object
-function spinner(div, slots) {
+// Create an svg directly in the div, initialize and return the spinner object
+function spinit(div, slots) {
   div.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" ' +
           'class="spin" width="100%" height="100%" viewbox="0 0 100 100"></svg>'
-
-  const spin = {
-    slots: slots,
-    obj: div.querySelector('svg'),
-    winner: -1,
-    startTime: Date.now(), // initial time
-    
+  const so = {
+    obj:    div.querySelector('svg'),
+    slots:  slots,                // list of slots (see genslots)
+    windex: -1,                   // index of the winning slot
+    tini:   Date.now(),           // initial timestamp, when spinning starts
+  //tdel:   4500,                 // total duration of the spin, in milliseconds
+  //vcur:   0,                    // current position in degrees
+    // ------------------------ we should be able to get rid of everything below
     speed: 0,
     spinSpeed: 1,
     degree: 0,
@@ -275,33 +273,29 @@ function spinner(div, slots) {
     max_duration: 5000,
     rand_speed: 0,
     duration: 0,
-
-    // trying a new thing here with fixed duration and only tracking position
-    //dur: 4500, // total duration of the spin
-    //pos: 0, 
   }
-
-  updateSpinner(spin, slots)
-  return spin
+  spinup(so, slots)
+  return so
 }
 
-// computes two slots given a probability
-function compute2Slots(p) {
-  if (p < 0 || p > 1) throw `Probability (${p}) must be between 0 and 1`
+// Generate a list of slots from a probability or list of probabilities.
+// For the case of 2 slots the first is for yes/pay/high/green and the second
+// is for no/free/low/red.
+function genslots(p) {
+  if (Array.isArray(p)) { 
+    ASSERT(false, "More than 2 slots not supported yet")
+    return null
+  }
+  if (!(p >= 0 && p <= 1)) {
+    return [
+      { value: "🍌", weight: 1, kyoom: 1, color: 'black' }, // Obviously we'll
+      { value: "🍒", weight: 0, kyoom: 1, color: 'taupe' }, // never see the 🍒
+    ]
+  }
   return [
-    // Or we could label the slices with 'YES/💰/⬆️' and 'NO/🆓/⬇️'
-    { value: percentify(p),   weight: p,   color: 'green' },
-    { value: percentify(1-p), weight: 1-p, color: 'red'   },
-  ]
-}
-
-window.spinner = {
-  create: spinner,
-  start: spin_start,
-  compute2Slots: compute2Slots,
-  update: updateSpinner,
-  setNaN: setSpinnerNaN,
-  redraw: redrawSpinner,
+    { value: percentify(p),   weight: p,   kyoom: p, color: 'green' },
+    { value: percentify(1-p), weight: 1-p, kyoom: 1, color: 'red'   },
+  ]  
 }
 
 // -----------------------------------------------------------------------------
